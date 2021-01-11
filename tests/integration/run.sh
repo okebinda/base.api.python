ec3657f2ec3ac8c5c906c7e6dbd35ae55d3319a0
Kevin Chevalier <kchevalier@protonmail.com>
2021-01-10 19:57:14 -0800

    add tagging support to circleci config

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -18,12 +18,6 @@
 export PIPENV_PIPFILE='/vagrant/application/Pipfile'
 export COVERAGE_FILE='/vagrant/tests/integration/.coverage'
 
-# export env variables
-cd /vagrant/application
-set -o allexport
-source config/.env.test
-set +o allexport
-
 # flags
 FLAG_COVERAGE_REPORT='false'
 FLAG_COVERAGE_HTML='false'
@@ -40,7 +34,7 @@ shift $((OPTIND-1))
 
 # run integration tests
 cd /vagrant/application/src/main/python
-pipenv run coverage run -m --source=. pytest -m integration ../../pytest/python/
+PIPENV_DOTENV_LOCATION=/vagrant/application/config/.env.test pipenv run coverage run -m --source=. pytest -m integration ../../pytest/python/
 
 # show coverage
 if [ "$FLAG_COVERAGE_REPORT" = true ]
